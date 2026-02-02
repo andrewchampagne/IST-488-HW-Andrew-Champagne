8778bc8db840b13e565135a95af45ca31678f744
andrewchampagne <114505093+andrewchampagne@users.noreply.github.com>
2026-02-01 21:35:48 -0500

    changed names

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-lab1 = st.Page("pages/HW1.py", title="HW1")
-lab2 = st.Page("pages/HW2.py", title="HW2",  default=True)
+hw1 = st.Page("pages/HW1.py", title="HW1")
+hw2 = st.Page("pages/HW2.py", title="HW2",  default=True)
 
 # Create navigation
-pg = st.navigation([lab1, lab2])
+pg = st.navigation([hw1, hw2])
 
 
 pg.run()
