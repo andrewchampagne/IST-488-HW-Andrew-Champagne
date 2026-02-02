b40152034366d29577463e28965c469b45b1de38
andrewchampagne <114505093+andrewchampagne@users.noreply.github.com>
2026-02-01 22:01:22 -0500

    changed models

--- pages/HW2.py
+++ pages/HW2.py
@@ -33,11 +33,12 @@ llm_provider = st.sidebar.selectbox(
 )
 
 use_advanced_model = st.sidebar.checkbox("Use advanced model")
+	
 
 if llm_provider == "OpenAI":
     model = "gpt-4o" if use_advanced_model else "gpt-4o-mini"
 else:  # Groq
-    model = "deepseek-r1-distill-llama-70b" if use_advanced_model else "llama-3.3-70b-versatile"
+    model = "llama-3.3-70b-versatile" if use_advanced_model else "whisper-large-v3-turbo"
 
 st.sidebar.write(f"Current model: {model}")
 
